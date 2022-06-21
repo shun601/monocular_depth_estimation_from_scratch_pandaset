64b2e43eeb8a99891109ee075b7cea33f8337f12
shun601 <shun.osaka.1989@gmail.com>
2022-06-22 01:14:05 +0900

    refactor

--- src/dataset.py
+++ src/dataset.py
@@ -13,85 +13,19 @@ import cv2
 import numpy as np
 import pandas as pd
 import transforms3d as t3d
-
-from .sequence import Sequence
-from .utils import subdirectories
-
-
-class DataSet:
-    """Top-level class to load PandaSet
-        ``DataSet`` prepares and loads ``Sequence`` objects for every sequence found in provided directory.
-        Access to a specific sequence is provided by using the sequence name as a key on the ``DataSet`` object.
-        Args:
-             directory: Absolute or relative path where PandaSet has been extracted to.
-        Examples:
-            >>> pandaset = DataSet('/data/pandaset')
-            >>> s = pandaset['002']
-        """
-
-    def __init__(self, directory: str) -> None:
-        self._directory: str = directory
-        self._sequences: Dict[str, Sequence] = None
-        self._load_sequences()
-
-    def __getitem__(self, item) -> Sequence:
-        return self._sequences[item]
-
-    def _load_sequences(self) -> None:
-        self._sequences = {}
-        sequence_directories = subdirectories(self._directory)
-        for sd in sequence_directories:
-            seq_id = sd.split('/')[-1].split('\\')[-1]
-            self._sequences[seq_id] = Sequence(sd)
-
-    def sequences(self, with_semseg: bool = False) -> List[str]:
-        """ Lists all available sequence names
-        Args:
-            with_semseg: Set `True` if only sequences with semantic segmentation annotations should be returned. Set `False` to return all sequences (with or without semantic segmentation).
-        Returns:
-            List of sequence names.
-        Examples:
-            >>> pandaset = DataSet('/data/pandaset')
-            >>> print(pandaset.sequences())
-            ['002','004','080']
-        """
-        if with_semseg:
-            return [s for s in list(self._sequences.keys()) if self._sequences[s].semseg]
-        else:
-            return list(self._sequences.keys())
-
-    def unload(self, sequence: str):
-        """ Removes all sequence file data from memory if previously loaded from disk.
-        This is useful if you intend to iterate over all sequences and perform some
-        operation. If you do not unload the sequences, it quickly leads to sigkill.
-        Args:
-            sequence: The sequence name
-        Returns:
-            None
-        Examples:
-            >>> pandaset = DataSet('...')
-            >>> for sequence in pandaset.sequences():
-            >>>     seq = pandaset[sequence]
-            >>>     seq.load()
-            >>>     # do operations on sequence here...
-            >>>     # when finished, unload the sequence from memory
-            >>>     pandaset.unload(sequence)
-        """
-        if sequence in self._sequences:
-            del self._sequences[sequence]
-            
+         
 
 class Pandaset(Dataset):
     def __init__(
         self,
-        root_dir="./data/",
-        variations=["camera", "lidar"]
+        root_dir="./data",
+        variations=["camera", "lidar"],
         cameras=["back_camera", "front_camera", "front_left_camera", "front_right_camera", "left_camera", "right_camera"],
         frame_inds=[-1, 0]):
 
         self.root_dir = Path(root_dir)
-        scenes = glob(f"{root_dir}/**")        
-        self.scenes = [os.path.split(scenes)[-1] for scene in scenes]
+        scenes = glob(f"{root_dir}/**")
+        self.scenes = [os.path.split(scene)[-1] for scene in scenes]
         self.variations = variations
         self.cameras = cameras
         self.frame_inds = frame_inds
@@ -101,7 +35,7 @@ class Pandaset(Dataset):
 
     def load_sequences(self):
         self.sequences = []
-        for idx in range()
+        for scene in self.scenes:
             for camera in self.cameras:
                 for variation in self.variations:
                     if variation == "camera":
@@ -111,8 +45,8 @@ class Pandaset(Dataset):
 
                         rgb_list = sorted(rgb_dir.glob("*.jpg"))
 
-                        extrinsics = pose_to_extrinsic_array(pose_json, int(scene))
-                        intrinsice = intrinsic_to_array(intrinsic_json_path)
+                        extrinsics = pose_to_extrinsic_array(pose_json_path, int(scene))
+                        intrinsics = intrinsic_to_array(intrinsic_json_path)
                         sequence = list(zip(rgb_list, extrinsics, intrinsics))
                         self.sequences.append(sequence)
                     elif variation == "lidar":
